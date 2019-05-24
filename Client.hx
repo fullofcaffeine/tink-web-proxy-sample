ad5cc53afb33e873b625cb0398f426192cdbee63
Marcelo de Moraes Serpa <boss@fullofcaffeine.com>
2019-05-24 18:44:03 -0500

    Completion issue

--- Client.hx
+++ Client.hx
@@ -5,11 +5,25 @@ import Server.Root;
 
 import ApiResult.Result;
 
+
+typedef Some = {
+  a: Int,
+  b: String
+}
+
 @:expose
 class Client {
     public static function main() {
         var remote = new Remote<Root>(new JsClient(), new RemoteEndpoint(new Host('localhost', 3000)));
-        var ret = remote.hello().handle((o) -> {
+        // Completion works for the var below. If you try
+        // bar. <-- it will correctly show the popup with `a` and `b`
+        var bar: Some = {a: 1, b: 'Foo'};
+        trace(bar.a);
+        
+        // Completion *does not work* for the remote object though:
+        // remote. <-- nothing happens
+
+        remote.hello().handle((o) -> {
           trace(o);
         });
     }
